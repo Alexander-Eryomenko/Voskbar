8ec60156aadf8ef1e521edda0a70803ade560c9d
Alexander-Eryomenko <sanyochek1991@gmail.com>
2021-02-13 00:15:54 +0200

    voskbar

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $('.promo__slider').slick({
+    // accessibility: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    // prevArrow: <button type="button" class="slick-prev">Previous</button>,
+    // nextArrow: <button type="button" class="slick-next">Next</button>,
+    // centerMode: true,
+    // centerPadding: '300px',
+    // cssEase: 'ease',
+    dots: false,
+    // dotsClass: 'slick-dots',
+    fade: true,
+    // focusOnSelect: true,
+    // respondTo: 'slider',
+    speed: 2000,
+
+  });
+});
