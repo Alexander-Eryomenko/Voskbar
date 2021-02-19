fbec772fca399c2343d4895b5d4da9a834d32dad
Alexander-Eryomenko <sanyochek1991@gmail.com>
2021-02-19 14:13:28 +0200

    header-top media

--- js/script.js
+++ js/script.js
@@ -28,3 +28,23 @@ $(document).ready(function(){
     speed: 2000,
   });
 });
+
+const btn = document.querySelector('.hamburger')
+const overlay = document.querySelector('.overlay')
+const menu = document.querySelector('.menu')
+const btnClose = document.querySelector('.btn-close')
+
+btn.addEventListener('click', (event) => {
+  overlay.classList.add('active')
+  menu.classList.add('active')
+})
+
+// btnClose.addEventListener('click', (event) => {
+//   overlay.classList.remove('active')
+//   menu.classList.remove('active')
+// })
+
+overlay.addEventListener('click', (event) => {
+  overlay.classList.remove('active')
+  menu.classList.remove('active')
+})
